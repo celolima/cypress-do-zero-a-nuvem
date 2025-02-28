b3cb3816fc2c298879850be51458a5cf8a3921b3
Marcelo Lima <celorsl@yahoo.com.br>
2025-02-28 15:54:03 -0300

    Adiciona testes de viewport mobile

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -143,6 +143,13 @@ describe('Central de Atendimento ao Cliente TAT', () => {
   })
 
   it.only('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', () => {
-    cy.get('a[href="privacy.html"]').should('have.attr', 'target', '_blank')
+    // Pode ser feito de duas formas
+    cy.get('a[href="privacy.html"]').should('have.attr', 'target', '_blank')    
+    cy.contains('a','Política de Privacidade').should('have.attr','href','privacy.html').and('have.attr', 'target', '_blank')
+  })
+
+  it.only('acessa a pagina de politica removendo o target e verifica seu titulo', () => {
+    cy.contains('a','Política de Privacidade').invoke('removeAttr','target').click()
+    cy.contains('h1','CAC TAT - Política de Privacidade').should('be.visible')
   })
 })
